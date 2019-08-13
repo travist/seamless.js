c5deffc6047b2506793f87bc41f143835eb0d3d7
maliknur <malik.dba.pro@gmail.com>
2019-08-13 12:11:31 -0700

    add build files

--- build/seamless.parent.js
+++ build/seamless.parent.js
@@ -983,15 +983,9 @@ try {
 
     // Add the necessary attributes.
     var attributes = {
-      'scrolling': 'no',
       'seamless': 'seamless',
       'width': '100%',
       'height': '0px',
-      'marginheight': '0',
-      'marginwidth': '0',
-      'frameborder': '0',
-      'horizontalscrolling': 'no',
-      'verticalscrolling': 'no',
       'style': 'border: none; overflow-y: hidden;'
     };
     for (var name in attributes) {
